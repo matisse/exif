ce8499ba79aac5cfc31ba645c50a4e6f19eb703d
Matisse Enzer <matisse@matisse.net>
2024-02-03 19:31:31 -0800

    Commit files missed in previous commit.

--- exif/Exif.swift
+++ exif/Exif.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import ArgumentParser
+import ImageIO
+
+@main
+struct Exif : ParsableCommand {
+
+    @Argument(help: "Path to an image file")
+    var path: String
+
+    mutating func run() throws {
+        let fullPath = URL(filePath: path).standardized
+        print("Path is:\(fullPath.path())")
+        
+        let options = [kCGImageSourceShouldCache : false]
+        guard let imageData = CGImageSourceCreateWithURL(fullPath as CFURL, options as CFDictionary) else {
+            throw "failed to read file \(fullPath.path())"
+        }
+
+        guard let metadata = CGImageSourceCopyMetadataAtIndex(imageData, 0, nil) else {
+            throw "Could not copy metadata from \(fullPath.path())"
+        }
+
+        print("Metadata: \(metadata)")
+    }
+}
+
+extension String : Error {
+    
+}
